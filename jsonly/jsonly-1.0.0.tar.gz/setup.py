17967f442b39f52b282d3ebb3f05aa52af5c96dd
Tom Forbes <tom@tomforb.es>
2022-12-02 12:51:25 +0000

    {"name":"jsonly","version":"1.0.0","file":"jsonly-1.0.0.tar.gz","path":"jsonly/jsonly-1.0.0.tar.gz"}

---
+++ jsonly/jsonly-1.0.0.tar.gz/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from jsonly import __version__
+
+setuptools.setup(
+    name="jsonly",
+    version=__version__,
+    license="MIT",
+    author="VoidAsMad",
+    author_email="voidasmad@gmail.com",
+    description="JSON 데이터 관리 라이브러리",
+    long_description=open("README.md", "rt", encoding="UTF8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/VoidAsMad/jsonly",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6"
+)
