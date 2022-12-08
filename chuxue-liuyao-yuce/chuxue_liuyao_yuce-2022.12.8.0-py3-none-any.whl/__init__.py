5810d1201499e04d618c1d752203300c0ef18401
Tom Forbes <tom@tomforb.es>
2022-12-08 10:06:17 +0000

    {"name":"chuxue-liuyao-yuce","version":"2022.12.8.0","file":"chuxue_liuyao_yuce-2022.12.8.0-py3-none-any.whl","path":"chuxue-liuyao-yuce/chuxue_liuyao_yuce-2022.12.8.0-py3-none-any.whl"}

---
+++ chuxue-liuyao-yuce/chuxue_liuyao_yuce-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chuxue-liuyao-yuce
+https://github.com/apachecn/chuxue-liuyao-yuce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
