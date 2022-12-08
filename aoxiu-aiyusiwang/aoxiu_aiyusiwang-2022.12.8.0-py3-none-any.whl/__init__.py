04dc6935cf44db7b99c9acf6555f489559eec166
Tom Forbes <tom@tomforb.es>
2022-12-08 09:41:22 +0000

    {"name":"aoxiu-aiyusiwang","version":"2022.12.8.0","file":"aoxiu_aiyusiwang-2022.12.8.0-py3-none-any.whl","path":"aoxiu-aiyusiwang/aoxiu_aiyusiwang-2022.12.8.0-py3-none-any.whl"}

---
+++ aoxiu-aiyusiwang/aoxiu_aiyusiwang-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-aiyusiwang
+https://github.com/apachecn/aoxiu-aiyusiwang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
