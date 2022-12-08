f655e5ec01cfd911eda2ae3186a11acfc60fe023
Tom Forbes <tom@tomforb.es>
2022-12-08 08:53:52 +0000

    {"name":"aoxiu-baiyun-zhidao","version":"2022.12.8.0","file":"aoxiu_baiyun_zhidao-2022.12.8.0-py3-none-any.whl","path":"aoxiu-baiyun-zhidao/aoxiu_baiyun_zhidao-2022.12.8.0-py3-none-any.whl"}

---
+++ aoxiu-baiyun-zhidao/aoxiu_baiyun_zhidao-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-baiyun-zhidao
+https://github.com/apachecn/aoxiu-baiyun-zhidao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
