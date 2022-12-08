9b90a6427ac92fdffcf6e16ee2d489af5c182716
Tom Forbes <tom@tomforb.es>
2022-12-08 09:05:41 +0000

    {"name":"aoxiu-zhuangzi-xinjie","version":"2022.12.8.0","file":"aoxiu_zhuangzi_xinjie-2022.12.8.0-py3-none-any.whl","path":"aoxiu-zhuangzi-xinjie/aoxiu_zhuangzi_xinjie-2022.12.8.0-py3-none-any.whl"}

---
+++ aoxiu-zhuangzi-xinjie/aoxiu_zhuangzi_xinjie-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-zhuangzi-xinjie
+https://github.com/apachecn/aoxiu-zhuangzi-xinjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
