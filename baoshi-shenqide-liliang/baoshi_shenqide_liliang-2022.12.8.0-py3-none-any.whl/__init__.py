ae87aff12690a11c084d5a396f8119ac0c518fa3
Tom Forbes <tom@tomforb.es>
2022-12-08 09:42:40 +0000

    {"name":"baoshi-shenqide-liliang","version":"2022.12.8.0","file":"baoshi_shenqide_liliang-2022.12.8.0-py3-none-any.whl","path":"baoshi-shenqide-liliang/baoshi_shenqide_liliang-2022.12.8.0-py3-none-any.whl"}

---
+++ baoshi-shenqide-liliang/baoshi_shenqide_liliang-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""baoshi-shenqide-liliang
+https://github.com/apachecn/baoshi-shenqide-liliang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
