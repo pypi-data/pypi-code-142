91af3423b40f5e0673475b5765b17d61acc29d23
Tom Forbes <tom@tomforb.es>
2022-12-08 09:18:58 +0000

    {"name":"nengliang-lingbai-zhanbu-shouce","version":"2022.12.8.0","file":"nengliang-lingbai-zhanbu-shouce-2022.12.8.0.tar.gz","path":"nengliang-lingbai-zhanbu-shouce/nengliang-lingbai-zhanbu-shouce-2022.12.8.0.tar.gz"}

---
+++ nengliang-lingbai-zhanbu-shouce/nengliang-lingbai-zhanbu-shouce-2022.12.8.0.tar.gz/NengliangLingbaiZhanbuShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nengliang-lingbai-zhanbu-shouce
+https://github.com/apachecn/nengliang-lingbai-zhanbu-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
