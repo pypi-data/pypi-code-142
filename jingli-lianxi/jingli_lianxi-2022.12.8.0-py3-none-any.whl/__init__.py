78cf68033030b80a5dba795ceb895d5526a772d7
Tom Forbes <tom@tomforb.es>
2022-12-08 09:49:07 +0000

    {"name":"jingli-lianxi","version":"2022.12.8.0","file":"jingli_lianxi-2022.12.8.0-py3-none-any.whl","path":"jingli-lianxi/jingli_lianxi-2022.12.8.0-py3-none-any.whl"}

---
+++ jingli-lianxi/jingli_lianxi-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingli-lianxi
+https://github.com/apachecn/jingli-lianxi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
