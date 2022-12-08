9bf47d6144f7abf67719dad2c9d034193e904744
Tom Forbes <tom@tomforb.es>
2022-12-08 10:16:22 +0000

    {"name":"ziwei-doushu-sixi-daci","version":"2022.12.8.0","file":"ziwei_doushu_sixi_daci-2022.12.8.0-py3-none-any.whl","path":"ziwei-doushu-sixi-daci/ziwei_doushu_sixi_daci-2022.12.8.0-py3-none-any.whl"}

---
+++ ziwei-doushu-sixi-daci/ziwei_doushu_sixi_daci-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-doushu-sixi-daci
+https://github.com/apachecn/ziwei-doushu-sixi-daci"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
