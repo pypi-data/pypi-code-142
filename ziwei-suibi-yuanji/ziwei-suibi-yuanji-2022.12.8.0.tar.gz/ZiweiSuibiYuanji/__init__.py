af3a0bc6d79914418192dac489c2624779f1e4d8
Tom Forbes <tom@tomforb.es>
2022-12-08 10:17:02 +0000

    {"name":"ziwei-suibi-yuanji","version":"2022.12.8.0","file":"ziwei-suibi-yuanji-2022.12.8.0.tar.gz","path":"ziwei-suibi-yuanji/ziwei-suibi-yuanji-2022.12.8.0.tar.gz"}

---
+++ ziwei-suibi-yuanji/ziwei-suibi-yuanji-2022.12.8.0.tar.gz/ZiweiSuibiYuanji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-suibi-yuanji
+https://github.com/apachecn/ziwei-suibi-yuanji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
