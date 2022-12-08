52eaec7acfdbbfc06f48f98f456082537659d6eb
Tom Forbes <tom@tomforb.es>
2022-12-08 10:15:48 +0000

    {"name":"zhijue-zhimen","version":"2022.12.8.0","file":"zhijue-zhimen-2022.12.8.0.tar.gz","path":"zhijue-zhimen/zhijue-zhimen-2022.12.8.0.tar.gz"}

---
+++ zhijue-zhimen/zhijue-zhimen-2022.12.8.0.tar.gz/ZhijueZhimen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhijue-zhimen
+https://github.com/apachecn/zhijue-zhimen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
