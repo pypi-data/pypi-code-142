3c1df2b8b86807f25da9d821dc8fb99d5874188e
Tom Forbes <tom@tomforb.es>
2022-12-08 09:44:31 +0000

    {"name":"daqi-zhongyi","version":"2022.12.8.0","file":"daqi-zhongyi-2022.12.8.0.tar.gz","path":"daqi-zhongyi/daqi-zhongyi-2022.12.8.0.tar.gz"}

---
+++ daqi-zhongyi/daqi-zhongyi-2022.12.8.0.tar.gz/DaqiZhongyi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""daqi-zhongyi
+https://github.com/apachecn/daqi-zhongyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
