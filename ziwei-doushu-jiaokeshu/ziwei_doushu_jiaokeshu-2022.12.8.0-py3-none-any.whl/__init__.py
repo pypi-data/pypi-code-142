2e9f3a4c73bc068b284a78950f14b7435c388858
Tom Forbes <tom@tomforb.es>
2022-12-08 10:38:54 +0000

    {"name":"ziwei-doushu-jiaokeshu","version":"2022.12.8.0","file":"ziwei_doushu_jiaokeshu-2022.12.8.0-py3-none-any.whl","path":"ziwei-doushu-jiaokeshu/ziwei_doushu_jiaokeshu-2022.12.8.0-py3-none-any.whl"}

---
+++ ziwei-doushu-jiaokeshu/ziwei_doushu_jiaokeshu-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-doushu-jiaokeshu
+https://github.com/apachecn/ziwei-doushu-jiaokeshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
