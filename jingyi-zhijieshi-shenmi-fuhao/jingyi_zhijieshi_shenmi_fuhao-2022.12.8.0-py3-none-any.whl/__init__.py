3bdc130c798ab70fe3857e5f4a42ef8b73f192e7
Tom Forbes <tom@tomforb.es>
2022-12-08 09:49:27 +0000

    {"name":"jingyi-zhijieshi-shenmi-fuhao","version":"2022.12.8.0","file":"jingyi_zhijieshi_shenmi_fuhao-2022.12.8.0-py3-none-any.whl","path":"jingyi-zhijieshi-shenmi-fuhao/jingyi_zhijieshi_shenmi_fuhao-2022.12.8.0-py3-none-any.whl"}

---
+++ jingyi-zhijieshi-shenmi-fuhao/jingyi_zhijieshi_shenmi_fuhao-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingyi-zhijieshi-shenmi-fuhao
+https://github.com/apachecn/jingyi-zhijieshi-shenmi-fuhao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
