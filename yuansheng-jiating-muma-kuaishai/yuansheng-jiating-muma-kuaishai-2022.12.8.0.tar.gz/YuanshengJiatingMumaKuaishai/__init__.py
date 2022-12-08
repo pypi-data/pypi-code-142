51166946c5d3860da8f9616024f6ebd535e9190b
Tom Forbes <tom@tomforb.es>
2022-12-08 10:36:15 +0000

    {"name":"yuansheng-jiating-muma-kuaishai","version":"2022.12.8.0","file":"yuansheng-jiating-muma-kuaishai-2022.12.8.0.tar.gz","path":"yuansheng-jiating-muma-kuaishai/yuansheng-jiating-muma-kuaishai-2022.12.8.0.tar.gz"}

---
+++ yuansheng-jiating-muma-kuaishai/yuansheng-jiating-muma-kuaishai-2022.12.8.0.tar.gz/YuanshengJiatingMumaKuaishai/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yuansheng-jiating-muma-kuaishai
+https://github.com/apachecn/yuansheng-jiating-muma-kuaishai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
