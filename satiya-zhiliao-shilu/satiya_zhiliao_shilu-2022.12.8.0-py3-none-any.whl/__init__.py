43328f675b5bc48399d311b6b03fe11fa351a049
Tom Forbes <tom@tomforb.es>
2022-12-08 10:19:53 +0000

    {"name":"satiya-zhiliao-shilu","version":"2022.12.8.0","file":"satiya_zhiliao_shilu-2022.12.8.0-py3-none-any.whl","path":"satiya-zhiliao-shilu/satiya_zhiliao_shilu-2022.12.8.0-py3-none-any.whl"}

---
+++ satiya-zhiliao-shilu/satiya_zhiliao_shilu-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""satiya-zhiliao-shilu
+https://github.com/apachecn/satiya-zhiliao-shilu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
