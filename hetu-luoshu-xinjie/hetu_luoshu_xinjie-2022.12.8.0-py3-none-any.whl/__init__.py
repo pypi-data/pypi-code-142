c9a2805d4b4760833d4bc6e95b81f53a4544d87e
Tom Forbes <tom@tomforb.es>
2022-12-08 09:11:57 +0000

    {"name":"hetu-luoshu-xinjie","version":"2022.12.8.0","file":"hetu_luoshu_xinjie-2022.12.8.0-py3-none-any.whl","path":"hetu-luoshu-xinjie/hetu_luoshu_xinjie-2022.12.8.0-py3-none-any.whl"}

---
+++ hetu-luoshu-xinjie/hetu_luoshu_xinjie-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hetu-luoshu-xinjie
+https://github.com/apachecn/hetu-luoshu-xinjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
