1652ade227e0603394e44f4de8408966b3edb78f
Tom Forbes <tom@tomforb.es>
2022-12-08 09:51:33 +0000

    {"name":"liangzi-tianming","version":"2022.12.8.1","file":"liangzi_tianming-2022.12.8.1-py3-none-any.whl","path":"liangzi-tianming/liangzi_tianming-2022.12.8.1-py3-none-any.whl"}

---
+++ liangzi-tianming/liangzi_tianming-2022.12.8.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liangzi-tianming
+https://github.com/apachecn/liangzi-tianming"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.1"
