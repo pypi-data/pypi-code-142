b43d60bd75892996acead25472a7d50b188f47b5
Tom Forbes <tom@tomforb.es>
2022-12-08 10:06:47 +0000

    {"name":"yeli-xinlixue","version":"2022.12.8.0","file":"yeli-xinlixue-2022.12.8.0.tar.gz","path":"yeli-xinlixue/yeli-xinlixue-2022.12.8.0.tar.gz"}

---
+++ yeli-xinlixue/yeli-xinlixue-2022.12.8.0.tar.gz/YeliXinlixue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yeli-xinlixue
+https://github.com/apachecn/yeli-xinlixue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
