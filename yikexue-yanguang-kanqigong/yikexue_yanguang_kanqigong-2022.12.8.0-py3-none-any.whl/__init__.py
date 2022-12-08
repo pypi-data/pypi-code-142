b8dcacdee19b921da90ca08b5138f7ba2d10d1f6
Tom Forbes <tom@tomforb.es>
2022-12-08 09:26:54 +0000

    {"name":"yikexue-yanguang-kanqigong","version":"2022.12.8.0","file":"yikexue_yanguang_kanqigong-2022.12.8.0-py3-none-any.whl","path":"yikexue-yanguang-kanqigong/yikexue_yanguang_kanqigong-2022.12.8.0-py3-none-any.whl"}

---
+++ yikexue-yanguang-kanqigong/yikexue_yanguang_kanqigong-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yikexue-yanguang-kanqigong
+https://github.com/apachecn/yikexue-yanguang-kanqigong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
