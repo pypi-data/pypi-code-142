af956039141deaebca987ebc81e023d44b58ae0b
Tom Forbes <tom@tomforb.es>
2022-12-08 09:15:58 +0000

    {"name":"liuyao-yuce-wuzhongwu","version":"2022.12.8.0","file":"liuyao_yuce_wuzhongwu-2022.12.8.0-py3-none-any.whl","path":"liuyao-yuce-wuzhongwu/liuyao_yuce_wuzhongwu-2022.12.8.0-py3-none-any.whl"}

---
+++ liuyao-yuce-wuzhongwu/liuyao_yuce_wuzhongwu-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liuyao-yuce-wuzhongwu
+https://github.com/apachecn/liuyao-yuce-wuzhongwu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
