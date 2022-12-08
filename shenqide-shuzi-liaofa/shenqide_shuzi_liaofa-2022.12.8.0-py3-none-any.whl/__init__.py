f191936c79f0ef5c8735bb3a523fefea83987500
Tom Forbes <tom@tomforb.es>
2022-12-08 09:57:31 +0000

    {"name":"shenqide-shuzi-liaofa","version":"2022.12.8.0","file":"shenqide_shuzi_liaofa-2022.12.8.0-py3-none-any.whl","path":"shenqide-shuzi-liaofa/shenqide_shuzi_liaofa-2022.12.8.0-py3-none-any.whl"}

---
+++ shenqide-shuzi-liaofa/shenqide_shuzi_liaofa-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenqide-shuzi-liaofa
+https://github.com/apachecn/shenqide-shuzi-liaofa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
