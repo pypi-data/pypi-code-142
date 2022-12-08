8d2aa9015536aa6b9e8714e552f6bb197cfdaebc
Tom Forbes <tom@tomforb.es>
2022-12-08 10:11:41 +0000

    {"name":"kaimen-zhuzai-shuzi-gaibian-mingyun","version":"2022.12.8.0","file":"kaimen-zhuzai-shuzi-gaibian-mingyun-2022.12.8.0.tar.gz","path":"kaimen-zhuzai-shuzi-gaibian-mingyun/kaimen-zhuzai-shuzi-gaibian-mingyun-2022.12.8.0.tar.gz"}

---
+++ kaimen-zhuzai-shuzi-gaibian-mingyun/kaimen-zhuzai-shuzi-gaibian-mingyun-2022.12.8.0.tar.gz/KaimenZhuzaiShuziGaibianMingyun/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kaimen-zhuzai-shuzi-gaibian-mingyun
+https://github.com/apachecn/kaimen-zhuzai-shuzi-gaibian-mingyun"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
