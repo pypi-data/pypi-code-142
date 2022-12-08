40e3bf8e5d07e635d6b00a9422d2a90bb035e6db
Tom Forbes <tom@tomforb.es>
2022-12-08 09:23:04 +0000

    {"name":"shena-nidaodi-zaibangwo-shenme","version":"2022.12.8.0","file":"shena-nidaodi-zaibangwo-shenme-2022.12.8.0.tar.gz","path":"shena-nidaodi-zaibangwo-shenme/shena-nidaodi-zaibangwo-shenme-2022.12.8.0.tar.gz"}

---
+++ shena-nidaodi-zaibangwo-shenme/shena-nidaodi-zaibangwo-shenme-2022.12.8.0.tar.gz/ShenaNidaodiZaibangwoShenme/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shena-nidaodi-zaibangwo-shenme
+https://github.com/apachecn/shena-nidaodi-zaibangwo-shenme"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
