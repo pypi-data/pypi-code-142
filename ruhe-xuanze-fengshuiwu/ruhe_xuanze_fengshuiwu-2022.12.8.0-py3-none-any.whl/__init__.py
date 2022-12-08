865ab2ca35bc9af5e9a34f059b58ff604da6b010
Tom Forbes <tom@tomforb.es>
2022-12-08 10:17:26 +0000

    {"name":"ruhe-xuanze-fengshuiwu","version":"2022.12.8.0","file":"ruhe_xuanze_fengshuiwu-2022.12.8.0-py3-none-any.whl","path":"ruhe-xuanze-fengshuiwu/ruhe_xuanze_fengshuiwu-2022.12.8.0-py3-none-any.whl"}

---
+++ ruhe-xuanze-fengshuiwu/ruhe_xuanze_fengshuiwu-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ruhe-xuanze-fengshuiwu
+https://github.com/apachecn/ruhe-xuanze-fengshuiwu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
