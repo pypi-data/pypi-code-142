0088ede9de3efae40ede9c4b67e2cc3e1295d8fc
Tom Forbes <tom@tomforb.es>
2022-12-08 10:16:30 +0000

    {"name":"qimen-dunjia-gaoji-yanxiuban-jiangyi","version":"2022.12.8.0","file":"qimen_dunjia_gaoji_yanxiuban_jiangyi-2022.12.8.0-py3-none-any.whl","path":"qimen-dunjia-gaoji-yanxiuban-jiangyi/qimen_dunjia_gaoji_yanxiuban_jiangyi-2022.12.8.0-py3-none-any.whl"}

---
+++ qimen-dunjia-gaoji-yanxiuban-jiangyi/qimen_dunjia_gaoji_yanxiuban_jiangyi-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qimen-dunjia-gaoji-yanxiuban-jiangyi
+https://github.com/apachecn/qimen-dunjia-gaoji-yanxiuban-jiangyi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
