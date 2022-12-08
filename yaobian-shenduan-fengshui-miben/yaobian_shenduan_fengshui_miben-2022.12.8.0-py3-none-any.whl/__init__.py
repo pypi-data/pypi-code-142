e84d47386504eca8ffbbe1d640c75f6d9c4d04e7
Tom Forbes <tom@tomforb.es>
2022-12-08 10:06:28 +0000

    {"name":"yaobian-shenduan-fengshui-miben","version":"2022.12.8.0","file":"yaobian_shenduan_fengshui_miben-2022.12.8.0-py3-none-any.whl","path":"yaobian-shenduan-fengshui-miben/yaobian_shenduan_fengshui_miben-2022.12.8.0-py3-none-any.whl"}

---
+++ yaobian-shenduan-fengshui-miben/yaobian_shenduan_fengshui_miben-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yaobian-shenduan-fengshui-miben
+https://github.com/apachecn/yaobian-shenduan-fengshui-miben"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
