92b99d0d0cf1d2976c3f0889ec2eb4cd73f4fad3
Tom Forbes <tom@tomforb.es>
2022-12-08 09:45:32 +0000

    {"name":"fangliao-sushen","version":"2022.12.8.0","file":"fangliao_sushen-2022.12.8.0-py3-none-any.whl","path":"fangliao-sushen/fangliao_sushen-2022.12.8.0-py3-none-any.whl"}

---
+++ fangliao-sushen/fangliao_sushen-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fangliao-sushen
+https://github.com/apachecn/fangliao-sushen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
