a43e0f6a25dbdb268c3d2ce66d4146e51b3695c8
Tom Forbes <tom@tomforb.es>
2022-12-08 09:11:31 +0000

    {"name":"guang-weilaide-yixue","version":"2022.12.8.0","file":"guang_weilaide_yixue-2022.12.8.0-py3-none-any.whl","path":"guang-weilaide-yixue/guang_weilaide_yixue-2022.12.8.0-py3-none-any.whl"}

---
+++ guang-weilaide-yixue/guang_weilaide_yixue-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""guang-weilaide-yixue
+https://github.com/apachecn/guang-weilaide-yixue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
