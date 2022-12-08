4712f508701c21d6c4ba9fa0b67ead64024ab2f2
Tom Forbes <tom@tomforb.es>
2022-12-08 09:59:55 +0000

    {"name":"wode-tongling-jingyan","version":"2022.12.8.0","file":"wode-tongling-jingyan-2022.12.8.0.tar.gz","path":"wode-tongling-jingyan/wode-tongling-jingyan-2022.12.8.0.tar.gz"}

---
+++ wode-tongling-jingyan/wode-tongling-jingyan-2022.12.8.0.tar.gz/WodeTonglingJingyan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wode-tongling-jingyan
+https://github.com/apachecn/wode-tongling-jingyan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
