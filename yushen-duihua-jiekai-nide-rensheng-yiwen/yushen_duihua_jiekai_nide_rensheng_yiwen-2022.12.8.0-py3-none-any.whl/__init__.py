624dd319ebe5b98e52f7caa3b2191af8bea90377
Tom Forbes <tom@tomforb.es>
2022-12-08 10:36:37 +0000

    {"name":"yushen-duihua-jiekai-nide-rensheng-yiwen","version":"2022.12.8.0","file":"yushen_duihua_jiekai_nide_rensheng_yiwen-2022.12.8.0-py3-none-any.whl","path":"yushen-duihua-jiekai-nide-rensheng-yiwen/yushen_duihua_jiekai_nide_rensheng_yiwen-2022.12.8.0-py3-none-any.whl"}

---
+++ yushen-duihua-jiekai-nide-rensheng-yiwen/yushen_duihua_jiekai_nide_rensheng_yiwen-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yushen-duihua-jiekai-nide-rensheng-yiwen
+https://github.com/apachecn/yushen-duihua-jiekai-nide-rensheng-yiwen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
