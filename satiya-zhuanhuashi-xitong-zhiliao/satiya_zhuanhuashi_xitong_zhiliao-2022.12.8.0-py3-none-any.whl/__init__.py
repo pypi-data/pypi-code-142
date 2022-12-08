1e77dc9dce3235eb6127b288fab6b8eb0c2935ed
Tom Forbes <tom@tomforb.es>
2022-12-08 10:20:06 +0000

    {"name":"satiya-zhuanhuashi-xitong-zhiliao","version":"2022.12.8.0","file":"satiya_zhuanhuashi_xitong_zhiliao-2022.12.8.0-py3-none-any.whl","path":"satiya-zhuanhuashi-xitong-zhiliao/satiya_zhuanhuashi_xitong_zhiliao-2022.12.8.0-py3-none-any.whl"}

---
+++ satiya-zhuanhuashi-xitong-zhiliao/satiya_zhuanhuashi_xitong_zhiliao-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""satiya-zhuanhuashi-xitong-zhiliao
+https://github.com/apachecn/satiya-zhuanhuashi-xitong-zhiliao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
