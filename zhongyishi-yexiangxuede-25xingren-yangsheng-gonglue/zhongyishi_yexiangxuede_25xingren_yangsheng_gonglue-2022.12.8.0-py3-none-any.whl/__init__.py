b28cf5b33364b954e54360833f390e2fa5648121
Tom Forbes <tom@tomforb.es>
2022-12-08 10:37:45 +0000

    {"name":"zhongyishi-yexiangxuede-25xingren-yangsheng-gonglue","version":"2022.12.8.0","file":"zhongyishi_yexiangxuede_25xingren_yangsheng_gonglue-2022.12.8.0-py3-none-any.whl","path":"zhongyishi-yexiangxuede-25xingren-yangsheng-gonglue/zhongyishi_yexiangxuede_25xingren_yangsheng_gonglue-2022.12.8.0-py3-none-any.whl"}

---
+++ zhongyishi-yexiangxuede-25xingren-yangsheng-gonglue/zhongyishi_yexiangxuede_25xingren_yangsheng_gonglue-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhongyishi-yexiangxuede-25xingren-yangsheng-gonglue
+https://github.com/apachecn/zhongyishi-yexiangxuede-25xingren-yangsheng-gonglue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
