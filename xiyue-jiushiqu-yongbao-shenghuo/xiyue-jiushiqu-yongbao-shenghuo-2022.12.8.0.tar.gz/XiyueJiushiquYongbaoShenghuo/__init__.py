2796427746394be3e1cc52f0742bb610ad4b790c
Tom Forbes <tom@tomforb.es>
2022-12-08 10:34:46 +0000

    {"name":"xiyue-jiushiqu-yongbao-shenghuo","version":"2022.12.8.0","file":"xiyue-jiushiqu-yongbao-shenghuo-2022.12.8.0.tar.gz","path":"xiyue-jiushiqu-yongbao-shenghuo/xiyue-jiushiqu-yongbao-shenghuo-2022.12.8.0.tar.gz"}

---
+++ xiyue-jiushiqu-yongbao-shenghuo/xiyue-jiushiqu-yongbao-shenghuo-2022.12.8.0.tar.gz/XiyueJiushiquYongbaoShenghuo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiyue-jiushiqu-yongbao-shenghuo
+https://github.com/apachecn/xiyue-jiushiqu-yongbao-shenghuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
