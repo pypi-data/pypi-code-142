d00525601d993380b27afc25ca0f9c3334d5e84f
Tom Forbes <tom@tomforb.es>
2022-12-08 09:04:05 +0000

    {"name":"aoxiu-shenghuo-zhihui-fangqingongxie-yixiu-chanshi","version":"2022.12.8.0","file":"aoxiu_shenghuo_zhihui_fangqingongxie_yixiu_chanshi-2022.12.8.0-py3-none-any.whl","path":"aoxiu-shenghuo-zhihui-fangqingongxie-yixiu-chanshi/aoxiu_shenghuo_zhihui_fangqingongxie_yixiu_chanshi-2022.12.8.0-py3-none-any.whl"}

---
+++ aoxiu-shenghuo-zhihui-fangqingongxie-yixiu-chanshi/aoxiu_shenghuo_zhihui_fangqingongxie_yixiu_chanshi-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-shenghuo-zhihui-fangqingongxie-yixiu-chanshi
+https://github.com/apachecn/aoxiu-shenghuo-zhihui-fangqingongxie-yixiu-chanshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
