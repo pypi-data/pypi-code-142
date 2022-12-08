fd05e9bd26da1724e1c225341a1205ac14550102
Tom Forbes <tom@tomforb.es>
2022-12-08 10:10:32 +0000

    {"name":"jingyi-shijieshi-kongbu-mori-yuyan","version":"2022.12.8.0","file":"jingyi-shijieshi-kongbu-mori-yuyan-2022.12.8.0.tar.gz","path":"jingyi-shijieshi-kongbu-mori-yuyan/jingyi-shijieshi-kongbu-mori-yuyan-2022.12.8.0.tar.gz"}

---
+++ jingyi-shijieshi-kongbu-mori-yuyan/jingyi-shijieshi-kongbu-mori-yuyan-2022.12.8.0.tar.gz/JingyiShijieshiKongbuMoriYuyan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jingyi-shijieshi-kongbu-mori-yuyan
+https://github.com/apachecn/jingyi-shijieshi-kongbu-mori-yuyan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
