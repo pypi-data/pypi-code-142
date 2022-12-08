3d3d57f130508b1e9f5eb62b8d7421dec48a2a66
Tom Forbes <tom@tomforb.es>
2022-12-08 08:53:15 +0000

    {"name":"ailv-jingyoude-love-anmo","version":"2022.12.8.0","file":"ailv-jingyoude-love-anmo-2022.12.8.0.tar.gz","path":"ailv-jingyoude-love-anmo/ailv-jingyoude-love-anmo-2022.12.8.0.tar.gz"}

---
+++ ailv-jingyoude-love-anmo/ailv-jingyoude-love-anmo-2022.12.8.0.tar.gz/AilvJingyoudeLoveAnmo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ailv-jingyoude-love-anmo
+https://github.com/apachecn/ailv-jingyoude-love-anmo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
