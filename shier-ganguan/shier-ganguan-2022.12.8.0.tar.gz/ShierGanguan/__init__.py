037a18c1661f7eeaa9028b7c99f06c29e54a73f7
Tom Forbes <tom@tomforb.es>
2022-12-08 10:21:40 +0000

    {"name":"shier-ganguan","version":"2022.12.8.0","file":"shier-ganguan-2022.12.8.0.tar.gz","path":"shier-ganguan/shier-ganguan-2022.12.8.0.tar.gz"}

---
+++ shier-ganguan/shier-ganguan-2022.12.8.0.tar.gz/ShierGanguan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shier-ganguan
+https://github.com/apachecn/shier-ganguan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
