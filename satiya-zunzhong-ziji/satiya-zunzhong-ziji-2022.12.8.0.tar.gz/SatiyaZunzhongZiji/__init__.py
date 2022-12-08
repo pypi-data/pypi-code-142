dd33ec62348cfa1e0c54cfc42ecb0d77298d2a55
Tom Forbes <tom@tomforb.es>
2022-12-08 09:56:46 +0000

    {"name":"satiya-zunzhong-ziji","version":"2022.12.8.0","file":"satiya-zunzhong-ziji-2022.12.8.0.tar.gz","path":"satiya-zunzhong-ziji/satiya-zunzhong-ziji-2022.12.8.0.tar.gz"}

---
+++ satiya-zunzhong-ziji/satiya-zunzhong-ziji-2022.12.8.0.tar.gz/SatiyaZunzhongZiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""satiya-zunzhong-ziji
+https://github.com/apachecn/satiya-zunzhong-ziji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
