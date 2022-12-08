22265329ce8a3a34d0b46e078ccd17c1bb5a3df6
Tom Forbes <tom@tomforb.es>
2022-12-08 10:27:42 +0000

    {"name":"xingfu-nvrende-fangxiang-shenghuo","version":"2022.12.8.0","file":"xingfu_nvrende_fangxiang_shenghuo-2022.12.8.0-py3-none-any.whl","path":"xingfu-nvrende-fangxiang-shenghuo/xingfu_nvrende_fangxiang_shenghuo-2022.12.8.0-py3-none-any.whl"}

---
+++ xingfu-nvrende-fangxiang-shenghuo/xingfu_nvrende_fangxiang_shenghuo-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xingfu-nvrende-fangxiang-shenghuo
+https://github.com/apachecn/xingfu-nvrende-fangxiang-shenghuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
