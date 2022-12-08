2ec8943864babdc3dfb49337cff96a4d1e0bf9b5
Tom Forbes <tom@tomforb.es>
2022-12-08 10:07:22 +0000

    {"name":"yepiaoran-daliuren-jiangyi-zhong","version":"2022.12.8.0","file":"yepiaoran-daliuren-jiangyi-zhong-2022.12.8.0.tar.gz","path":"yepiaoran-daliuren-jiangyi-zhong/yepiaoran-daliuren-jiangyi-zhong-2022.12.8.0.tar.gz"}

---
+++ yepiaoran-daliuren-jiangyi-zhong/yepiaoran-daliuren-jiangyi-zhong-2022.12.8.0.tar.gz/YepiaoranDaliurenJiangyiZhong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yepiaoran-daliuren-jiangyi-zhong
+https://github.com/apachecn/yepiaoran-daliuren-jiangyi-zhong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
