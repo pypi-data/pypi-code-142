0b30d63487271ea587ee91b2d87941bcb449d7f8
Tom Forbes <tom@tomforb.es>
2022-12-08 10:00:03 +0000

    {"name":"women-doushi-waixingren","version":"2022.12.8.0","file":"women_doushi_waixingren-2022.12.8.0-py3-none-any.whl","path":"women-doushi-waixingren/women_doushi_waixingren-2022.12.8.0-py3-none-any.whl"}

---
+++ women-doushi-waixingren/women_doushi_waixingren-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""women-doushi-waixingren
+https://github.com/apachecn/women-doushi-waixingren"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
