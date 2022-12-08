680b8b545753b7f9a3632c5e01ab49d7956ba603
Tom Forbes <tom@tomforb.es>
2022-12-08 09:43:30 +0000

    {"name":"caiguang-nengliang-ziran-liaofa","version":"2022.12.8.0","file":"caiguang-nengliang-ziran-liaofa-2022.12.8.0.tar.gz","path":"caiguang-nengliang-ziran-liaofa/caiguang-nengliang-ziran-liaofa-2022.12.8.0.tar.gz"}

---
+++ caiguang-nengliang-ziran-liaofa/caiguang-nengliang-ziran-liaofa-2022.12.8.0.tar.gz/CaiguangNengliangZiranLiaofa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""caiguang-nengliang-ziran-liaofa
+https://github.com/apachecn/caiguang-nengliang-ziran-liaofa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
