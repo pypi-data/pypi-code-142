877996c83979b1065c696b4c3fe86a40f243cec1
Tom Forbes <tom@tomforb.es>
2022-12-08 10:09:48 +0000

    {"name":"jiaju-fengshui-100ji","version":"2022.12.8.0","file":"jiaju-fengshui-100ji-2022.12.8.0.tar.gz","path":"jiaju-fengshui-100ji/jiaju-fengshui-100ji-2022.12.8.0.tar.gz"}

---
+++ jiaju-fengshui-100ji/jiaju-fengshui-100ji-2022.12.8.0.tar.gz/JiajuFengshui100ji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiaju-fengshui-100ji
+https://github.com/apachecn/jiaju-fengshui-100ji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
