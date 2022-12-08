3e6f80ba1d4909af146e1a5e708e4d55d619cdcc
Tom Forbes <tom@tomforb.es>
2022-12-08 10:39:41 +0000

    {"name":"ziwei-doushu-taiwei-mingpan","version":"2022.12.8.0","file":"ziwei_doushu_taiwei_mingpan-2022.12.8.0-py3-none-any.whl","path":"ziwei-doushu-taiwei-mingpan/ziwei_doushu_taiwei_mingpan-2022.12.8.0-py3-none-any.whl"}

---
+++ ziwei-doushu-taiwei-mingpan/ziwei_doushu_taiwei_mingpan-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-doushu-taiwei-mingpan
+https://github.com/apachecn/ziwei-doushu-taiwei-mingpan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
