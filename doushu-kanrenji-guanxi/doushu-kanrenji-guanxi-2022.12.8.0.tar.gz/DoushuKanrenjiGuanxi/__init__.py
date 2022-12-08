2e00cf6cfa4996cc1a0d460921e2eae1bc0af7b0
Tom Forbes <tom@tomforb.es>
2022-12-08 09:44:58 +0000

    {"name":"doushu-kanrenji-guanxi","version":"2022.12.8.0","file":"doushu-kanrenji-guanxi-2022.12.8.0.tar.gz","path":"doushu-kanrenji-guanxi/doushu-kanrenji-guanxi-2022.12.8.0.tar.gz"}

---
+++ doushu-kanrenji-guanxi/doushu-kanrenji-guanxi-2022.12.8.0.tar.gz/DoushuKanrenjiGuanxi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""doushu-kanrenji-guanxi
+https://github.com/apachecn/doushu-kanrenji-guanxi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
