ae8affcbd1d0be2fe4b32fd61b4292bd9a830886
Tom Forbes <tom@tomforb.es>
2022-12-08 09:12:49 +0000

    {"name":"jiuxing-renge-dongxi-renxingde-xinling-mima","version":"2022.12.8.0","file":"jiuxing-renge-dongxi-renxingde-xinling-mima-2022.12.8.0.tar.gz","path":"jiuxing-renge-dongxi-renxingde-xinling-mima/jiuxing-renge-dongxi-renxingde-xinling-mima-2022.12.8.0.tar.gz"}

---
+++ jiuxing-renge-dongxi-renxingde-xinling-mima/jiuxing-renge-dongxi-renxingde-xinling-mima-2022.12.8.0.tar.gz/JiuxingRengeDongxiRenxingdeXinlingMima/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiuxing-renge-dongxi-renxingde-xinling-mima
+https://github.com/apachecn/jiuxing-renge-dongxi-renxingde-xinling-mima"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
