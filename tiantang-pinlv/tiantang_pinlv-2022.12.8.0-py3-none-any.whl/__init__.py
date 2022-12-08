bd10aee24807bf958506ce8994f3fdc3f875d99c
Tom Forbes <tom@tomforb.es>
2022-12-08 09:59:10 +0000

    {"name":"tiantang-pinlv","version":"2022.12.8.0","file":"tiantang_pinlv-2022.12.8.0-py3-none-any.whl","path":"tiantang-pinlv/tiantang_pinlv-2022.12.8.0-py3-none-any.whl"}

---
+++ tiantang-pinlv/tiantang_pinlv-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tiantang-pinlv
+https://github.com/apachecn/tiantang-pinlv"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
