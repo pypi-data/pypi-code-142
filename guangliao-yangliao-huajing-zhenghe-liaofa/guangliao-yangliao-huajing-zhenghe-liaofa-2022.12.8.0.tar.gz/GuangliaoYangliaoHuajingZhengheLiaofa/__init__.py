5e4d8fbd694ec89db79f1356792ca7f646b85173
Tom Forbes <tom@tomforb.es>
2022-12-08 09:47:55 +0000

    {"name":"guangliao-yangliao-huajing-zhenghe-liaofa","version":"2022.12.8.0","file":"guangliao-yangliao-huajing-zhenghe-liaofa-2022.12.8.0.tar.gz","path":"guangliao-yangliao-huajing-zhenghe-liaofa/guangliao-yangliao-huajing-zhenghe-liaofa-2022.12.8.0.tar.gz"}

---
+++ guangliao-yangliao-huajing-zhenghe-liaofa/guangliao-yangliao-huajing-zhenghe-liaofa-2022.12.8.0.tar.gz/GuangliaoYangliaoHuajingZhengheLiaofa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""guangliao-yangliao-huajing-zhenghe-liaofa
+https://github.com/apachecn/guangliao-yangliao-huajing-zhenghe-liaofa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
