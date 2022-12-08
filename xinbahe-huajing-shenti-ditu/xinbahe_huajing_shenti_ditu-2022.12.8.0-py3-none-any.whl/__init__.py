3c834da5f7e14ef6020dc0108b378e9a7e6542a8
Tom Forbes <tom@tomforb.es>
2022-12-08 10:02:34 +0000

    {"name":"xinbahe-huajing-shenti-ditu","version":"2022.12.8.0","file":"xinbahe_huajing_shenti_ditu-2022.12.8.0-py3-none-any.whl","path":"xinbahe-huajing-shenti-ditu/xinbahe_huajing_shenti_ditu-2022.12.8.0-py3-none-any.whl"}

---
+++ xinbahe-huajing-shenti-ditu/xinbahe_huajing_shenti_ditu-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xinbahe-huajing-shenti-ditu
+https://github.com/apachecn/xinbahe-huajing-shenti-ditu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
