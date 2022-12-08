70583c292f3d356f55d7f31774237780c85e0f79
Tom Forbes <tom@tomforb.es>
2022-12-08 10:18:14 +0000

    {"name":"satiya-jiating-zhiliao-moshi","version":"2022.12.8.0","file":"satiya_jiating_zhiliao_moshi-2022.12.8.0-py3-none-any.whl","path":"satiya-jiating-zhiliao-moshi/satiya_jiating_zhiliao_moshi-2022.12.8.0-py3-none-any.whl"}

---
+++ satiya-jiating-zhiliao-moshi/satiya_jiating_zhiliao_moshi-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""satiya-jiating-zhiliao-moshi
+https://github.com/apachecn/satiya-jiating-zhiliao-moshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
