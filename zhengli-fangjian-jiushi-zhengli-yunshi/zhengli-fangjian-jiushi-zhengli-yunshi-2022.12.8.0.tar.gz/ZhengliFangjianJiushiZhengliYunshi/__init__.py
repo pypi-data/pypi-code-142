b14eeb1d17753ec307d76df3c3cd175b79aba360
Tom Forbes <tom@tomforb.es>
2022-12-08 10:37:00 +0000

    {"name":"zhengli-fangjian-jiushi-zhengli-yunshi","version":"2022.12.8.0","file":"zhengli-fangjian-jiushi-zhengli-yunshi-2022.12.8.0.tar.gz","path":"zhengli-fangjian-jiushi-zhengli-yunshi/zhengli-fangjian-jiushi-zhengli-yunshi-2022.12.8.0.tar.gz"}

---
+++ zhengli-fangjian-jiushi-zhengli-yunshi/zhengli-fangjian-jiushi-zhengli-yunshi-2022.12.8.0.tar.gz/ZhengliFangjianJiushiZhengliYunshi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhengli-fangjian-jiushi-zhengli-yunshi
+https://github.com/apachecn/zhengli-fangjian-jiushi-zhengli-yunshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
