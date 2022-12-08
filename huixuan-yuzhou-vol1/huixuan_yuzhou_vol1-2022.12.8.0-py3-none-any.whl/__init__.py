41671de5a77b43a0df24cd2e7058726265ac535c
Tom Forbes <tom@tomforb.es>
2022-12-08 09:48:17 +0000

    {"name":"huixuan-yuzhou-vol1","version":"2022.12.8.0","file":"huixuan_yuzhou_vol1-2022.12.8.0-py3-none-any.whl","path":"huixuan-yuzhou-vol1/huixuan_yuzhou_vol1-2022.12.8.0-py3-none-any.whl"}

---
+++ huixuan-yuzhou-vol1/huixuan_yuzhou_vol1-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huixuan-yuzhou-vol1
+https://github.com/apachecn/huixuan-yuzhou-vol1"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
