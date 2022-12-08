261056f8613a4b55595dc53a17b1a0a32236be3c
Tom Forbes <tom@tomforb.es>
2022-12-08 10:11:21 +0000

    {"name":"jujia-fengshui-daquan","version":"2022.12.8.0","file":"jujia_fengshui_daquan-2022.12.8.0-py3-none-any.whl","path":"jujia-fengshui-daquan/jujia_fengshui_daquan-2022.12.8.0-py3-none-any.whl"}

---
+++ jujia-fengshui-daquan/jujia_fengshui_daquan-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jujia-fengshui-daquan
+https://github.com/apachecn/jujia-fengshui-daquan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
