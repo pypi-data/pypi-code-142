96a9cd406974e8158bb54b1e54c3be7fdc80ea53
Tom Forbes <tom@tomforb.es>
2022-12-08 10:19:11 +0000

    {"name":"satiya-xinjiating-ruhe-suzaoren","version":"2022.12.8.0","file":"satiya_xinjiating_ruhe_suzaoren-2022.12.8.0-py3-none-any.whl","path":"satiya-xinjiating-ruhe-suzaoren/satiya_xinjiating_ruhe_suzaoren-2022.12.8.0-py3-none-any.whl"}

---
+++ satiya-xinjiating-ruhe-suzaoren/satiya_xinjiating_ruhe_suzaoren-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""satiya-xinjiating-ruhe-suzaoren
+https://github.com/apachecn/satiya-xinjiating-ruhe-suzaoren"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
