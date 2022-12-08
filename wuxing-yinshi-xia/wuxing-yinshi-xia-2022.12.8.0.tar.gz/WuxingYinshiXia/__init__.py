1d107c87e35c61fdded6a7cf8f86c1ccba653acb
Tom Forbes <tom@tomforb.es>
2022-12-08 10:25:57 +0000

    {"name":"wuxing-yinshi-xia","version":"2022.12.8.0","file":"wuxing-yinshi-xia-2022.12.8.0.tar.gz","path":"wuxing-yinshi-xia/wuxing-yinshi-xia-2022.12.8.0.tar.gz"}

---
+++ wuxing-yinshi-xia/wuxing-yinshi-xia-2022.12.8.0.tar.gz/WuxingYinshiXia/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wuxing-yinshi-xia
+https://github.com/apachecn/wuxing-yinshi-xia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
