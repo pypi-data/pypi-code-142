9a33579fe94e9301b5f6814f31cef19403cdcb3a
Tom Forbes <tom@tomforb.es>
2022-12-08 10:23:53 +0000

    {"name":"tianna-buhuishi-kadao-apiaoba","version":"2022.12.8.0","file":"tianna_buhuishi_kadao_apiaoba-2022.12.8.0-py3-none-any.whl","path":"tianna-buhuishi-kadao-apiaoba/tianna_buhuishi_kadao_apiaoba-2022.12.8.0-py3-none-any.whl"}

---
+++ tianna-buhuishi-kadao-apiaoba/tianna_buhuishi_kadao_apiaoba-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tianna-buhuishi-kadao-apiaoba
+https://github.com/apachecn/tianna-buhuishi-kadao-apiaoba"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
