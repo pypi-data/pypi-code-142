33fb13e135eabe90bfb6b0add37afab821f61ec4
Tom Forbes <tom@tomforb.es>
2022-12-08 10:16:17 +0000

    {"name":"qiji-kecheng-xuyan-xinglv","version":"2022.12.8.0","file":"qiji_kecheng_xuyan_xinglv-2022.12.8.0-py3-none-any.whl","path":"qiji-kecheng-xuyan-xinglv/qiji_kecheng_xuyan_xinglv-2022.12.8.0-py3-none-any.whl"}

---
+++ qiji-kecheng-xuyan-xinglv/qiji_kecheng_xuyan_xinglv-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qiji-kecheng-xuyan-xinglv
+https://github.com/apachecn/qiji-kecheng-xuyan-xinglv"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
