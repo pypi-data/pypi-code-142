dcdc353c4559f9a63057179163e2c620e5a0ebdf
Tom Forbes <tom@tomforb.es>
2022-12-08 09:57:43 +0000

    {"name":"shenxing-heyide-qimeng-chuanqi","version":"2022.12.8.0","file":"shenxing_heyide_qimeng_chuanqi-2022.12.8.0-py3-none-any.whl","path":"shenxing-heyide-qimeng-chuanqi/shenxing_heyide_qimeng_chuanqi-2022.12.8.0-py3-none-any.whl"}

---
+++ shenxing-heyide-qimeng-chuanqi/shenxing_heyide_qimeng_chuanqi-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenxing-heyide-qimeng-chuanqi
+https://github.com/apachecn/shenxing-heyide-qimeng-chuanqi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
