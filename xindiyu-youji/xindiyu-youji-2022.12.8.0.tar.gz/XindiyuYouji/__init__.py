3b45a950acb51d33be40ed69ba799fbd49442c85
Tom Forbes <tom@tomforb.es>
2022-12-08 10:27:32 +0000

    {"name":"xindiyu-youji","version":"2022.12.8.0","file":"xindiyu-youji-2022.12.8.0.tar.gz","path":"xindiyu-youji/xindiyu-youji-2022.12.8.0.tar.gz"}

---
+++ xindiyu-youji/xindiyu-youji-2022.12.8.0.tar.gz/XindiyuYouji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xindiyu-youji
+https://github.com/apachecn/xindiyu-youji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
