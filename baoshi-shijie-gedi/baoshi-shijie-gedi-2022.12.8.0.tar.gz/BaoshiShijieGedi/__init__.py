4eee34e6262edf525afbe9ab34821dd9784bace6
Tom Forbes <tom@tomforb.es>
2022-12-08 10:02:10 +0000

    {"name":"baoshi-shijie-gedi","version":"2022.12.8.0","file":"baoshi-shijie-gedi-2022.12.8.0.tar.gz","path":"baoshi-shijie-gedi/baoshi-shijie-gedi-2022.12.8.0.tar.gz"}

---
+++ baoshi-shijie-gedi/baoshi-shijie-gedi-2022.12.8.0.tar.gz/BaoshiShijieGedi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""baoshi-shijie-gedi
+https://github.com/apachecn/baoshi-shijie-gedi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
