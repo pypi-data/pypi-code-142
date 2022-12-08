465b9538bc8c6de82003be547259400421480ec7
Tom Forbes <tom@tomforb.es>
2022-12-08 09:03:31 +0000

    {"name":"aoxiu-shadezhihui","version":"2022.12.8.0","file":"aoxiu_shadezhihui-2022.12.8.0-py3-none-any.whl","path":"aoxiu-shadezhihui/aoxiu_shadezhihui-2022.12.8.0-py3-none-any.whl"}

---
+++ aoxiu-shadezhihui/aoxiu_shadezhihui-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-shadezhihui
+https://github.com/apachecn/aoxiu-shadezhihui"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
