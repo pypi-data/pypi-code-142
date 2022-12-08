78080a078f47a078aa52072be089ad2e412b3ef1
Tom Forbes <tom@tomforb.es>
2022-12-08 09:09:39 +0000

    {"name":"fanshu-jiuneng-suanziwei","version":"2022.12.8.0","file":"fanshu-jiuneng-suanziwei-2022.12.8.0.tar.gz","path":"fanshu-jiuneng-suanziwei/fanshu-jiuneng-suanziwei-2022.12.8.0.tar.gz"}

---
+++ fanshu-jiuneng-suanziwei/fanshu-jiuneng-suanziwei-2022.12.8.0.tar.gz/FanshuJiunengSuanziwei/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""fanshu-jiuneng-suanziwei
+https://github.com/apachecn/fanshu-jiuneng-suanziwei"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
