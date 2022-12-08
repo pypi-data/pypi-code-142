9f3cdf08025c57da83a6666e07b422183f5d466a
Tom Forbes <tom@tomforb.es>
2022-12-08 09:16:56 +0000

    {"name":"meili-nvrende-fangxiang-shenghuo","version":"2022.12.8.0","file":"meili-nvrende-fangxiang-shenghuo-2022.12.8.0.tar.gz","path":"meili-nvrende-fangxiang-shenghuo/meili-nvrende-fangxiang-shenghuo-2022.12.8.0.tar.gz"}

---
+++ meili-nvrende-fangxiang-shenghuo/meili-nvrende-fangxiang-shenghuo-2022.12.8.0.tar.gz/MeiliNvrendeFangxiangShenghuo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""meili-nvrende-fangxiang-shenghuo
+https://github.com/apachecn/meili-nvrende-fangxiang-shenghuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
