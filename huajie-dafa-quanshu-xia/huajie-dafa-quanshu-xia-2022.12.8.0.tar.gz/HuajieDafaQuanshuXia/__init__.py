28168f7d8c4b04acbab5929bf82e182780415619
Tom Forbes <tom@tomforb.es>
2022-12-08 10:09:31 +0000

    {"name":"huajie-dafa-quanshu-xia","version":"2022.12.8.0","file":"huajie-dafa-quanshu-xia-2022.12.8.0.tar.gz","path":"huajie-dafa-quanshu-xia/huajie-dafa-quanshu-xia-2022.12.8.0.tar.gz"}

---
+++ huajie-dafa-quanshu-xia/huajie-dafa-quanshu-xia-2022.12.8.0.tar.gz/HuajieDafaQuanshuXia/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huajie-dafa-quanshu-xia
+https://github.com/apachecn/huajie-dafa-quanshu-xia"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
