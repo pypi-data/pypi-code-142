6cbab9f351875e9e85310ee9c52569f304830fa3
Tom Forbes <tom@tomforb.es>
2022-12-08 09:11:19 +0000

    {"name":"gebaini-wenti-xia","version":"2022.12.8.0","file":"gebaini-wenti-xia-2022.12.8.0.tar.gz","path":"gebaini-wenti-xia/gebaini-wenti-xia-2022.12.8.0.tar.gz"}

---
+++ gebaini-wenti-xia/gebaini-wenti-xia-2022.12.8.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import GebainiWentiXia
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('GebainiWentiXia'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="gebaini-wenti-xia",
+    version=GebainiWentiXia.__version__,
+    url="https://github.com/apachecn/gebaini-wenti-xia",
+    author=GebainiWentiXia.__author__,
+    author_email=GebainiWentiXia.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="哥白尼问题下",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "gebaini-wenti-xia=GebainiWentiXia.__main__:main",
+            "GebainiWentiXia=GebainiWentiXia.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
