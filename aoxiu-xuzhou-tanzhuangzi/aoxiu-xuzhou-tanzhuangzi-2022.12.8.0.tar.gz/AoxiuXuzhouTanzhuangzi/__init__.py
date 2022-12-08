2fe8e23f253897286838f8dd2c0319044db6e009
Tom Forbes <tom@tomforb.es>
2022-12-08 09:04:56 +0000

    {"name":"aoxiu-xuzhou-tanzhuangzi","version":"2022.12.8.0","file":"aoxiu-xuzhou-tanzhuangzi-2022.12.8.0.tar.gz","path":"aoxiu-xuzhou-tanzhuangzi/aoxiu-xuzhou-tanzhuangzi-2022.12.8.0.tar.gz"}

---
+++ aoxiu-xuzhou-tanzhuangzi/aoxiu-xuzhou-tanzhuangzi-2022.12.8.0.tar.gz/AoxiuXuzhouTanzhuangzi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aoxiu-xuzhou-tanzhuangzi
+https://github.com/apachecn/aoxiu-xuzhou-tanzhuangzi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
