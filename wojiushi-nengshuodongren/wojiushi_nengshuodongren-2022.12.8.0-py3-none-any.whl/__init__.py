d612cf75936038329eb30bf9558740b8aa2ecb45
Tom Forbes <tom@tomforb.es>
2022-12-08 10:24:51 +0000

    {"name":"wojiushi-nengshuodongren","version":"2022.12.8.0","file":"wojiushi_nengshuodongren-2022.12.8.0-py3-none-any.whl","path":"wojiushi-nengshuodongren/wojiushi_nengshuodongren-2022.12.8.0-py3-none-any.whl"}

---
+++ wojiushi-nengshuodongren/wojiushi_nengshuodongren-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""wojiushi-nengshuodongren
+https://github.com/apachecn/wojiushi-nengshuodongren"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
