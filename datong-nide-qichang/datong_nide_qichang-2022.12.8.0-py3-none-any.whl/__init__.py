e9a416f28984856139a5ce9d6a4b268e69d9c409
Tom Forbes <tom@tomforb.es>
2022-12-08 09:07:41 +0000

    {"name":"datong-nide-qichang","version":"2022.12.8.0","file":"datong_nide_qichang-2022.12.8.0-py3-none-any.whl","path":"datong-nide-qichang/datong_nide_qichang-2022.12.8.0-py3-none-any.whl"}

---
+++ datong-nide-qichang/datong_nide_qichang-2022.12.8.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""datong-nide-qichang
+https://github.com/apachecn/datong-nide-qichang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.12.8.0"
